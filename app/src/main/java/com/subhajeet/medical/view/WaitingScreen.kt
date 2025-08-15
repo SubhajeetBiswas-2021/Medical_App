9c3769064b71f351da395968b0bbc2467fced8eb
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-16 02:09:24 +0530

    Commit

--- app/src/main/java/com/subhajeet/medical/view/WaitingScreen.kt
+++ app/src/main/java/com/subhajeet/medical/view/WaitingScreen.kt
@@ -1,18 +1,26 @@
 package com.subhajeet.medical.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.subhajeet.medical.view.nav.Routes
 import com.subhajeet.medical.viewModel.MyViewModel
 
+
 @Composable
-fun WaitingScreen(navController: NavController, userId: String,viewModel: MyViewModel?= hiltViewModel()) {
+fun WaitingScreen(navController: NavController?, userId: String,viewModel: MyViewModel?= hiltViewModel()) {
 
 
     val getUserByIdstate = viewModel?.getUserByIdstate?.collectAsState()
@@ -23,9 +31,41 @@ fun WaitingScreen(navController: NavController, userId: String,viewModel: MyView
         }
     }
 
+    val isApproved = getUserByIdstate?.value?.success?.user?.isApproved == 1
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text="Waiting Screen", modifier = Modifier.fillMaxSize())
+    if(isApproved){
+        navController?.navigate(Routes.HomeRoutes){
+
+                popUpTo(Routes.WaitingRoutes) { inclusive = true }
+
+        }
+        return // stop showing the waiting UI
     }
 
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text="Admin has not approved yet. Please wait 24 hours.", modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {viewModel?.getUserById(userId)}) {
+            Text(text="Refresh")
+        }
+    }
+
+
+
+
+
+
 }
+
+@Preview(showBackground = true, showSystemUi=true)
+@Composable
+fun WaitingScreenPreview(){
+    WaitingScreen(
+        viewModel = null, navController = null,
+        userId = null.toString()
+    )
+}
+
