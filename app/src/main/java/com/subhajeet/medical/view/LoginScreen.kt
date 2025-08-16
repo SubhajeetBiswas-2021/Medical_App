3101251770fc418636a54a5c46145f0a762ca758
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-17 02:44:18 +0530

    Commit

--- app/src/main/java/com/subhajeet/medical/view/LoginScreen.kt
+++ app/src/main/java/com/subhajeet/medical/view/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -59,7 +60,7 @@ fun LoginScreen(navController: NavController?,viewModel: MyViewModel?= hiltViewM
         }
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize().padding(35.dp)) {
 
         OutlinedTextField(
             value = email.value,
