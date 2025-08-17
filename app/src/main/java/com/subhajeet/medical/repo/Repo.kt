08db6029598205023238a17fabf074b2503e2e25
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-17 14:05:43 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/medical/repo/Repo.kt
@@ -0,0 +1,49 @@
+package com.subhajeet.medical.repo
+
+import com.subhajeet.medical.Utils.ResultState
+import com.subhajeet.medical.models.ApiServices
+import com.subhajeet.medical.models.responseModels.LoginResponse
+import com.subhajeet.medical.models.responseModels.getUserByIdResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class Repo @Inject constructor(private val apiServices: ApiServices) {
+
+
+
+
+    //suspend fun login(email:String,password:String) = apiServices.login(email,password)
+    suspend fun login(email:String,password:String) : Flow<ResultState<LoginResponse>> = flow{
+
+        emit(ResultState.Loading)
+        try{
+            val response = apiServices.login(email,password)
+
+            if(response.isSuccessful && response.body() != null){
+                emit(ResultState.Success(response.body()!!))
+            }else{
+                emit(ResultState.Error(response.errorBody()?.string() ?: "Unknown error"))
+            }
+        }catch (e:Exception){
+            emit(ResultState.Error(e.message?:"An error message"))
+        }
+    }
+
+    suspend fun getUserById(userId:String) : Flow<ResultState<getUserByIdResponse>> = flow{
+
+        emit(ResultState.Loading)
+        try{
+            val response = apiServices.getUserById(userId)
+
+            if(response.isSuccessful && response.body() != null){
+                emit(ResultState.Success(response.body()!!))
+            }else{
+                emit(ResultState.Error(response.errorBody()?.string() ?: "Unknown error"))
+            }
+        }catch (e:Exception){
+            emit(ResultState.Error(e.message?:"An error message"))
+        }
+    }
+
+}
