e9373502944f459a29ac97126fffa877160bf7f2
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-18 22:42:43 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/medical/view/nav/Routes.kt
@@ -0,0 +1,20 @@
+package com.subhajeet.medical.view.nav
+
+import kotlinx.serialization.Serializable
+
+sealed class Routes {
+
+    @Serializable
+    object LoginRoutes
+
+    @Serializable
+    object SignUpRoutes
+
+
+    @Serializable
+    data class WaitingRoutes(val userId: String) //// Now it's a data class so we can pass userId
+
+
+    @Serializable
+    object HomeRoutes
+}
