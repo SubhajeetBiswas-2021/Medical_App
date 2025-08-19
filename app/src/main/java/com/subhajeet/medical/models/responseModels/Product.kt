de4ba52244e922921029e7968c2701ab0b18da33
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-19 19:39:02 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/medical/models/responseModels/Product.kt
@@ -0,0 +1,10 @@
+package com.subhajeet.medical.models.responseModels
+
+data class Product(
+    val category: String,
+    val id: Int,
+    val name: String,
+    val price: String,
+    val product_id: String,
+    val stock: String
+)
